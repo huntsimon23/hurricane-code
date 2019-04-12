3eea0f381ada66473b0cc90724f9c7c9270c576b
Jose <dreamzzediting@gmail.com>
2019-04-11 21:43:05 -0400

    updates

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+function initMap() {
+    // The location of Uluru
+    var uluru = {lat: -25.344, lng: 131.036};
+    // The map, centered at Uluru
+    var map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 16});
+    // The marker, positioned at Uluru
+}

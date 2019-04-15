ffe37526179a5a3ebc9615c4834d6011d10e2227
tblasberg <terena.blasberg@gmail.com>
2019-04-15 19:37:26 -0400

    commiting to pull

--- assets/js/fbAuth.js
+++ assets/js/fbAuth.js
@@ -1,1 +1,15 @@
 //This is where the firebase code goes
+
+<script src="https://www.gstatic.com/firebasejs/5.9.4/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCZShavrqBv1mczIBpUkG5Yj0oNeyulzec",
+    authDomain: "huntsimon23-disasterdashboard.firebaseapp.com",
+    databaseURL: "https://huntsimon23-disasterdashboard.firebaseio.com",
+    projectId: "huntsimon23-disasterdashboard",
+    storageBucket: "huntsimon23-disasterdashboard.appspot.com",
+    messagingSenderId: "1021340464037"
+  };
+  firebase.initializeApp(config);
+</script>

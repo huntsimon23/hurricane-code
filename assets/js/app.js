bde7db85f871d030658b641b0df6446525d7883d
Jose <dreamzzediting@gmail.com>
2019-04-17 21:21:11 -0400

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -114,6 +114,39 @@ $.ajax({
             .setLngLat([lat, long])
             .addTo(map);
 
+        // Create a popup, but don't add it to the map yet.
+        var popup = new mapboxgl.Popup({
+        closeButton: false,
+        closeOnClick: false
+        });
+         
+        map.on('mouseenter', function(e) {
+        // Change the cursor style as a UI indicator.
+        map.getCanvas().style.cursor = 'pointer';
+         
+        var coordinates = e.features[0].geometry.coordinates.slice();
+        var description = crisisCountries
+         
+        // Ensure that if the map is zoomed out such that multiple
+        // copies of the feature are visible, the popup appears
+        // over the copy being pointed to.
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+         
+        // Populate the popup and set its coordinates
+        // based on the feature found.
+        popup.setLngLat(coordinates)
+        .setHTML(description)
+        .addTo(map);
+        });
+         
+        map.on('mouseleave', 'places', function() {
+        map.getCanvas().style.cursor = '';
+        popup.remove();
+        }); 
+    
+
         })
 
     }
